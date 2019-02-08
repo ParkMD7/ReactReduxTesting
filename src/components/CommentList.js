f3a4371984d79ce9b9495735f7321033e37dc48d
ParkMD7 <parkermdinsmore@gmail.com>
2019-02-08 16:28:57 -0500

    commentlist component hooked up to redux store and now renders comments to the screen

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,6 +1,30 @@
 // dependencies
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default () => {
-  return <div>Comment List</div>
+class CommentList extends Component {
+
+  renderComments = () => {
+    return this.props.comments.map(comment => {
+      // assuming every comment is unique so using the comment itself as a key
+      return <li key={comment}>{comment}</li>
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.renderComments()}
+        </ul>
+      </div>
+    );
+  }
+
 }
+
+const mapStateToProps = (state) => ({
+  comments: state.comments
+})
+
+export default connect(mapStateToProps)(CommentList);
